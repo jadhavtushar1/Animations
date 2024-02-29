5844b7ac7841051c33ccb0a30b8acb924a7e0769
Tushar Jadhav <jadhavtusharr1@gmail.com>
2024-02-29 17:16:35 +0530

    implemented circle animation

---
+++ src/Components/Animations/CircleRotate/CircleRotate.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './CircleRotate.css'
+
+function CircleRotate() {
+  return (
+    <div className='center-flex'>
+        <div className='circle-main-container center-flex'>
+            <h4>Hover Here</h4>
+        </div>
+    </div>
+  )
+}
+
+export default CircleRotate
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CircleRotate from '../../Components/Animations/CircleRotate/CircleRotate'
+
+function HomePage() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default HomePage
